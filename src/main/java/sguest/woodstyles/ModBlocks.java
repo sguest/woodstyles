50a8d2ba54fe8295f39cf47530864b387ead465b
sguest <spencer.b.guest@gmail.com>
2019-07-07 22:37:49 -0500

    Add remaining crafting tables

--- src/main/java/sguest/woodstyles/ModBlocks.java
+++ src/main/java/sguest/woodstyles/ModBlocks.java
@@ -10,15 +10,23 @@ import net.minecraftforge.registries.ObjectHolder;
 @Mod.EventBusSubscriber(modid = Woodstyles.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 @ObjectHolder(Woodstyles.MOD_ID)
 public class ModBlocks {
-    public static final Block acacia_crafting_table = null;
+    public static final Block oak_crafting_table = null;
+    public static final Block spruce_crafting_table = null;
     public static final Block birch_crafting_table = null;
+    public static final Block jungle_crafting_table = null;
+    public static final Block acacia_crafting_table = null;
+    public static final Block dark_oak_crafting_table = null;
 
     @SubscribeEvent
     public static void registerBlocks(RegistryEvent.Register<Block> event)
     {
         event.getRegistry().registerAll(
+            new CraftingTableBlock(Block.Properties.from(Blocks.CRAFTING_TABLE)).setRegistryName(Woodstyles.MOD_ID, "oak_crafting_table"),
+            new CraftingTableBlock(Block.Properties.from(Blocks.CRAFTING_TABLE)).setRegistryName(Woodstyles.MOD_ID, "spruce_crafting_table"),
+            new CraftingTableBlock(Block.Properties.from(Blocks.CRAFTING_TABLE)).setRegistryName(Woodstyles.MOD_ID, "birch_crafting_table"),
+            new CraftingTableBlock(Block.Properties.from(Blocks.CRAFTING_TABLE)).setRegistryName(Woodstyles.MOD_ID, "jungle_crafting_table"),
             new CraftingTableBlock(Block.Properties.from(Blocks.CRAFTING_TABLE)).setRegistryName(Woodstyles.MOD_ID, "acacia_crafting_table"),
-            new CraftingTableBlock(Block.Properties.from(Blocks.CRAFTING_TABLE)).setRegistryName(Woodstyles.MOD_ID, "birch_crafting_table")
+            new CraftingTableBlock(Block.Properties.from(Blocks.CRAFTING_TABLE)).setRegistryName(Woodstyles.MOD_ID, "dark_oak_crafting_table")
         );
     }
 }
--- src/main/java/sguest/woodstyles/ModItems.java
+++ src/main/java/sguest/woodstyles/ModItems.java
@@ -10,15 +10,23 @@ import net.minecraftforge.registries.ObjectHolder;
 @Mod.EventBusSubscriber(modid = Woodstyles.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
 @ObjectHolder(Woodstyles.MOD_ID)
 public class ModItems {
-    public static final Item acacia_crafting_table = null;
+    public static final Item oak_crafting_table = null;
+    public static final Item spruce_crafting_table = null;
     public static final Item birch_crafting_table = null;
+    public static final Item jungle_crafting_table = null;
+    public static final Item acacia_crafting_table = null;
+    public static final Item dark_oak_crafting_table = null;
 
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event)
     {
         event.getRegistry().registerAll(
+            new BlockItem(ModBlocks.oak_crafting_table, new Item.Properties()).setRegistryName(Woodstyles.MOD_ID, "oak_crafting_table"),
+            new BlockItem(ModBlocks.spruce_crafting_table, new Item.Properties()).setRegistryName(Woodstyles.MOD_ID, "spruce_crafting_table"),
+            new BlockItem(ModBlocks.birch_crafting_table, new Item.Properties()).setRegistryName(Woodstyles.MOD_ID, "birch_crafting_table"),
+            new BlockItem(ModBlocks.jungle_crafting_table, new Item.Properties()).setRegistryName(Woodstyles.MOD_ID, "jungle_crafting_table"),
             new BlockItem(ModBlocks.acacia_crafting_table, new Item.Properties()).setRegistryName(Woodstyles.MOD_ID, "acacia_crafting_table"),
-            new BlockItem(ModBlocks.birch_crafting_table, new Item.Properties()).setRegistryName(Woodstyles.MOD_ID, "birch_crafting_table")
+            new BlockItem(ModBlocks.dark_oak_crafting_table, new Item.Properties()).setRegistryName(Woodstyles.MOD_ID, "dark_oak_crafting_table")
         );
     }
 }
