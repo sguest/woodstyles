14dbb34ee128ae47d63df3ac2b0da3e024051fdd
sguest <spencer.b.guest@gmail.com>
2019-07-11 23:07:21 -0500

    Creative tabs

--- src/main/java/sguest/woodstyles/ModItems.java
+++ src/main/java/sguest/woodstyles/ModItems.java
@@ -3,6 +3,7 @@ package sguest.woodstyles;
 import net.minecraft.block.Block;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -15,17 +16,17 @@ public class ModItems {
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event)
     {
-        registerItemType(ModBlocks.crafting_tables, "crafting_table", event);
-        registerItemType(ModBlocks.bookshelves, "bookshelf", event);
+        registerItemType(ModBlocks.crafting_tables, "crafting_table", ItemGroup.DECORATIONS, event);
+        registerItemType(ModBlocks.bookshelves, "bookshelf", ItemGroup.BUILDING_BLOCKS, event);
     }
 
-    private static void registerItemType(Map<WoodType, Block> blockMap, String baseName, RegistryEvent.Register<Item> event)
+    private static void registerItemType(Map<WoodType, Block> blockMap, String baseName, ItemGroup group, RegistryEvent.Register<Item> event)
     {
-        ArrayList<Item> items = new ArrayList<Item>();
+        ArrayList<Item> items = new ArrayList<>();
 
         for(WoodType type: WoodType.values())
         {
-            items.add(new BlockItem(blockMap.get(type), new Item.Properties()).setRegistryName(Woodstyles.MOD_ID, type + "_" + baseName));
+            items.add(new BlockItem(blockMap.get(type), new Item.Properties().group(group)).setRegistryName(Woodstyles.MOD_ID, type + "_" + baseName));
         }
 
         event.getRegistry().registerAll(items.toArray(new Item[items.size()]));
